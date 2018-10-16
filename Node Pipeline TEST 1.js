c69543b120e4e6cc05eef4b29d616b1ac72318fb
fandus13 <edgar.fando@roche.com>
2018-10-16 16:49:46 +0200

    add push ssh

--- Node Pipeline TEST 1.js
+++ Node Pipeline TEST 1.js
@@ -34,5 +34,10 @@ pipeline {
         archiveArtifacts artifacts: 'build/**/*', fingerprint: true, onlyIfSuccessful: true
         }
       }
+      stage('SSHPush_to_Apache') {
+        steps {
+          sshPublisher(publishers: [sshPublisherDesc(configName: 'Apache', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: 'sudo systemctl restart apache2', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/var/www/html', remoteDirectorySDF: false, removePrefix: '', sourceFiles: 'cd /var/lib/jenkins/jobs//Node\\ pipeline\\ TEST/builds/lastStableBuild/archive/build/**/*')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
+        }
+      }
     }
   }
