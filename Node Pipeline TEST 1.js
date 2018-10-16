ffc5a26b735a5b35fa2559fb53e56312444ba11e
fandus13 <edgar.fando@roche.com>
2018-10-16 17:21:16 +0200

    push

--- Node Pipeline TEST 1.js
+++ Node Pipeline TEST 1.js
@@ -37,10 +37,7 @@ pipeline {
 
       stage('SSHPush_to_Apache') {
         steps {
-          sshPublisher(publishers: [sshPublisherDesc(configName: 'Apache', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '''sudo cp -r /usr/Fandus/* /var/www/html/
-          sudo systemctl restart apache2
-          ''', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '/usr/Fandus/', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '/var/lib/jenkins/jobs//Node\\ pipeline\\ TEST/builds/lastStableBuild/archive/build/**/*')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
-        }
+      sshPublisher(publishers: [sshPublisherDesc(configName: 'Apache', transfers: [sshTransfer(cleanRemote: false, excludes: '', execCommand: '', execTimeout: 120000, flatten: false, makeEmptyDirs: false, noDefaultExcludes: false, patternSeparator: '[, ]+', remoteDirectory: '', remoteDirectorySDF: false, removePrefix: '', sourceFiles: '/build/**/*')], usePromotionTimestamp: false, useWorkspaceInPromotion: false, verbose: false)])
       }
     }
   }
